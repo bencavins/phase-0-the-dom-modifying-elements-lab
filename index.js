80b605d5f6da80e1f9d9e94d3e1f9747567fb6b8
Ben Cavins <bencavins@gmail.com>
2024-11-20 15:10:38 -0500

    changes

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+const main = document.querySelector('#main')
+// main.remove()
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.setAttribute('id', 'victory')
+newHeader.textContent = 'Bob\'s \nthe "champion"'
+console.log('Bob\'s \nthe \t"champion"')
+
+'/home/folder/myfile'
+'c:\\home\\folder\\file'
+
+main.appendChild(newHeader)
--- test/indexTest.js
+++ test/indexTest.js
@@ -3,7 +3,9 @@ require ( './helpers.js' );
 describe("index.html", () => {
   describe("after index.js is processed", () => {
     it("no longer has DOM node 'main#main'", () => {
-      expect(document.querySelector('main#main'), "Make sure you remove the <main> with id 'main'").to.not.exist
+      expect(
+        document.querySelector('main#main'), "Make sure you remove the <main> with id 'main'"
+      ).to.not.exist
     });
 
     it("has a 'newHeader' variable that points to an <h1> node", () => {
